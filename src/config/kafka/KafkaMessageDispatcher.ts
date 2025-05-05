8e740f47f1696f3a458107e729947af409063b3f
Jefferson Oliveira <jefferson.oliveira203@etec.sp.gov.br>
2025-05-05 20:38:58 -0300

    add error exception

---
+++ src/config/kafka/KafkaMessageDispatcher.ts
@@ -0,0 +1,20 @@
+import { Message } from "kafkajs";
+import ProducerService from "../../services/ProducerService";
+import KafkaMessageError from "./error/KafkaMessageError";
+
+class KafkaMessageDispatcher {
+    constructor(
+        private producerService: ProducerService
+    ){}
+
+    async dispatch(topicName: string, message: Message) {
+        try {
+            await this.producerService.ProduceMessage(topicName, message);
+        } catch(e) {
+            throw new KafkaMessageError("Falha ao enviar mensagem para o Kafka algum erro ocorreu!", "500");
+        }
+    }
+}
+
+const kafkaMessageDispatcher = new KafkaMessageDispatcher(new ProducerService());
+export default kafkaMessageDispatcher;
